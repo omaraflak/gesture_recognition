4471e356fe9dab466e18de2888e4629ab68fce87
Omar Aflak <aflakomar@hotmail.fr>
2018-01-22 14:57:42 -0800

    skin reco based on face color

--- skin_reco.py
+++ skin_reco.py
@@ -1,48 +1,60 @@
 import numpy as np
 import cv2
 
+def filter_skin(area, lower_range, upper_range):
+    skinkernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+    hsv = cv2.cvtColor(area, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower_range, upper_range)
+    mask = cv2.erode(mask, skinkernel, iterations = 1)
+    mask = cv2.dilate(mask, skinkernel, iterations = 1)
+    mask = cv2.GaussianBlur(mask, (15,15), 1)
+    result = cv2.bitwise_and(hsv, hsv, mask = mask)
+    result = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
+    return result
+
 def main():
-    # load haar pretrained file
+    # load haar file for face detection
     face_cascade = cv2.CascadeClassifier('haar/haarcascade_frontalface_default.xml')
 
-    # camera
+    # start camera
     cap = cv2.VideoCapture(0)
 
-    while(True):
-        # Capture frame-by-frame
-        ret, img = cap.read()
+    while(1):
+        # read frame from camera
+        ret, frame = cap.read()
 
-        # camera capture to grayscale for head detection
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # draw region of interest
+        cv2.rectangle(frame, (100,100), (300,300), (0,0,255), 1)
+        area = frame[100:300, 100:300]
+
+        # detect faces
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
         for (x,y,w,h) in faces:
-            cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-            roi = img[y:y+h, x:x+w]
-            hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+            roi = frame[y:y+h, x:x+w]
 
-            # preparing kmeans clustering with k=2 i.e. searching 2 most dominant colors
-            K = 2
-            h,w,c = hsv.shape
-            Z = hsv.reshape((h*w, c))
-            Z = np.float32(Z)
+            # get hsv color of the face
+            h,w,c = roi.shape
+            color = roi[int(h/2), int(w/2)]
+            bgr = np.uint8([[color]])
+            hsv = cv2.cvtColor(bgr, cv2.COLOR_BGR2HSV)
 
-            # apply kmeans on face
-            criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-            ret, label, center = cv2.kmeans(Z, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+            # define skin color range based on face color
+            lower_range = np.array(hsv-[10,100,100])
+            upper_range = np.array(hsv+[10,255,255])
 
-            # print most dominant HSV color
-            print(center[0].astype(int))
+            # filter region of interest with skin color range
+            result = filter_skin(area, lower_range, upper_range)
+            cv2.imshow('skin filter', result)
 
-        # display camera preview
-        cv2.imshow('camera', img)
+        cv2.imshow('camera', frame)
 
         key = cv2.waitKey(1)
         if key == ord('q'):
             break
 
-    # When everything done, release the capture
-    cap.release()
     cv2.destroyAllWindows()
-    
+
 if __name__ == '__main__':
     main()
