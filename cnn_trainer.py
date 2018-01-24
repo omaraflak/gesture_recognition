48581d201075d0bd5197e3bab27da830f67fe67f
Omar Aflak <aflakomar@gmail.com>
2018-01-23 17:34:12 -0800

    added export to tensorflow lite

--- cnn_trainer.py
+++ cnn_trainer.py
@@ -26,7 +26,7 @@ trainPath = 'gestures/train'
 testPath = 'gestures/test'
 
 # number of classes/categories of network output (e.g. car, chicken, human --> 3)
-nb_classes = 3
+nb_classes = 4
 
 # how many images to process before applying gradient correction
 batch_sz = 32
@@ -171,6 +171,30 @@ def read_model(network_path, network_model, network_weights):
 	model.load_weights(os.path.join(network_path, network_weights))
 	return model
 
+# export model for mobile devices (tensorflow lite)
+def export_model_for_mobile(saver, model, model_name, input_node_names, output_node_name):
+    tf.train.write_graph(K.get_session().graph_def, 'out', \
+        model_name + '_graph.pbtxt')
+
+    saver.save(K.get_session(), 'out/' + model_name + '.chkp')
+
+    freeze_graph.freeze_graph('out/' + model_name + '_graph.pbtxt', None, \
+        False, 'out/' + model_name + '.chkp', output_node_name, \
+        "save/restore_all", "save/Const:0", \
+        'out/frozen_' + model_name + '.pb', True, "")
+
+    input_graph_def = tf.GraphDef()
+    with tf.gfile.Open('out/frozen_' + model_name + '.pb', "rb") as f:
+        input_graph_def.ParseFromString(f.read())
+
+    output_graph_def = optimize_for_inference_lib.optimize_for_inference(
+            input_graph_def, input_node_names, [output_node_name],
+            tf.float32.as_datatype_enum)
+
+    with tf.gfile.FastGFile('out/tensorflow_lite_' + model_name + '.pb', "wb") as f:
+        f.write(output_graph_def.SerializeToString())
+
+
 def main():
     # generate data
     generate_data(trainPath)
@@ -183,6 +207,9 @@ def main():
     model = build_model()
     train(model, x_train, y_train, x_test, y_test)
     save_model(model, 'cache', 'architecture.json', 'weights.h5')
+
+    # Export model for tensorflow lite
+    export_model_for_mobile(tf.train.Saver(), model, 'tribe_convnet', ["conv2d_1_input"], "dense_2/Softmax")
 
     # Evaluate model on test data
     scores = model.evaluate(x_test, y_test, verbose=0)
